1b5c7432aadceda3854e5c51b1a1f2727f1d4697
Fabian Schmengler <fs@integer-net.de>
2020-09-05 20:27:47 +0200

    Apply updates from magento2-module-template to Travis configuration
    - coverage only once per test suite
    - do not rely on etc/install-config-mysql.travis.php.dist anymore
    - configure autoload-dev
    - allow building of pull requests
    - install Elasticsearch 7.6.2
    - test on PHP 7.4
    - allow failures on 2.4-develop, test on 2.4 stable

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 # mock mail
@@ -23,19 +23,29 @@ cd magento2
 
 # add composer package under test, composer require will trigger update/install
 composer config minimum-stability dev
-composer config repositories.travis_to_test git https://github.com/$TRAVIS_REPO_SLUG.git
-
+composer config repositories.travis_to_test git https://github.com/${TRAVIS_REPO_SLUG}.git
 if [ ! -z $TRAVIS_TAG  ]
 then
     composer require ${COMPOSER_PACKAGE_NAME}:${TRAVIS_TAG}
+elif [ ! -z $TRAVIS_PULL_REQUEST_BRANCH ]
+then
+    # For pull requests, use the remote repository
+    composer config repositories.travis_to_test git https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
+    composer require ${COMPOSER_PACKAGE_NAME}:dev-${TRAVIS_PULL_REQUEST_BRANCH}\#${TRAVIS_PULL_REQUEST_SHA}
 else
     composer require ${COMPOSER_PACKAGE_NAME}:dev-${TRAVIS_BRANCH}\#${TRAVIS_COMMIT}
 fi
 
+# Add tests/src to autoload-dev on project level
+php -r '$composer_json = json_decode(file_get_contents("composer.json"), true);
+$composer_json["autoload-dev"]["psr-4"]["IntegerNet\\GlobalCustomLayout\\"] = "vendor/integer-net/magento2-global-custom-layout/tests/src";
+file_put_contents("composer.json", json_encode($composer_json));'
+composer dumpautoload
+
 # prepare for test suite
 case $TEST_SUITE in
     integration)
-        cp vendor/$COMPOSER_PACKAGE_NAME/tests/Integration/phpunit.xml.dist dev/tests/integration/phpunit.xml
+        cp vendor/$COMPOSER_PACKAGE_NAME/tests/integration/phpunit.xml.dist dev/tests/integration/phpunit.xml
 
         cd dev/tests/integration
 
@@ -44,12 +54,15 @@ case $TEST_SUITE in
             SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION;
             CREATE DATABASE magento_integration_tests;
         '
-        cp etc/install-config-mysql.travis.php.dist etc/install-config-mysql.php
+        cp etc/install-config-mysql.php.dist etc/install-config-mysql.php
+        # Remove AMQP configuration
         sed -i '/amqp/d' etc/install-config-mysql.php
+        # Remove default root password
+        sed -i 's/123123q//' etc/install-config-mysql.php
 
         cd ../../..
-        ;;
+    ;;
     unit)
-        cp vendor/$COMPOSER_PACKAGE_NAME/tests/Unit/phpunit.xml.dist dev/tests/unit/phpunit.xml
+        cp vendor/$COMPOSER_PACKAGE_NAME/tests/unit/phpunit.xml.dist dev/tests/unit/phpunit.xml
     ;;
 esac
